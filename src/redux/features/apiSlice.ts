d4b5d08a60b11ea38977a2dd828f3717e2d649ef
jimmy <mac@macdeMacBook-Air.local>
2023-08-30 12:16:00 +0800

    fix:correct endpoint to the deploy one

--- src/redux/features/apiSlice.ts
+++ src/redux/features/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/api/" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://memodown.vercel.app/api/" }),
     tagTypes: ['Auth', "Notizs", 'Friends', "Messages"],
     endpoints: builder => ({})
 })
