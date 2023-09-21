6fc99cb804b0c22904643649433c5f6c8985e77f
jimmy <mac@macdeMacBook-Air.local>
2023-09-21 14:02:08 +0900

    correct url

--- src/redux/features/apiSlice.ts
+++ src/redux/features/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({ baseUrl: "memodown.vercel.app/" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
     tagTypes: ['Auth', "Notizs", 'Friends', "Messages"],
     endpoints: builder => ({})
 })
